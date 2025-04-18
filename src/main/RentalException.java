fd8fdc93e74d9e745ec81f6b9489d66f28f0c7d8
Wacharaphong Sutthuboriban <6731503032@lamduan.mfu.ac.th>
2025-04-18 22:13:14 +0700

    FinishedProject

--- src/main/RentalException.java
+++ src/main/RentalException.java
@@ -2,7 +2,7 @@
 /*
  Custom exception class for handling rental system specific errors.
  */
-class RentalException extends Exception {   
+class RentalException extends Exception {  //checked exception 
 
     public RentalException(String message) {
         super(message);
